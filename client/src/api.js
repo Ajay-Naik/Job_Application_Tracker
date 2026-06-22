729c95319925ea0202a13f3170b8503430b0a1f3
ajay naik <ajaynaik6853@gmail.com>
2026-06-22 23:12:07 +0530

    fix: correct updateApplication method to use PUT instead of UPDATE and standardize code formatting

--- client/src/api.js
+++ client/src/api.js
@@ -6,5 +6,5 @@ const API = axios.create({
 
 export const getApplications = () => API.get('/applications');
 export const createApplication = (data) => API.post('/applications', data);
-export const updateApplication = (id,data) => API.update(`/applications/${id}`, data);
+export const updateApplication = (id,data) => API.put(`/applications/${id}`, data);
 export const deleteApplication = (id) => API.delete(`/applications/${id}`);
--- server/server.js
+++ server/server.js
@@ -1,24 +1,25 @@
-const dns = require('dns');
-dns.setServers(['8.8.8.8', '8.8.4.4']);
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-dns.setDefaultResultOrder('ipv4first');
+dns.setDefaultResultOrder("ipv4first");
 
-mongoose.connect(process.env.MONGO_URL)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.error("MongoDB connection error:", err));
 
-app.get('/', (req, res) => {
-  res.send('Job Tracker API running');
+app.get("/", (req, res) => {
+  res.send("Job Tracker API running");
 });
 
 const PORT = process.env.PORT || 5000;
-const applicationRoutes = require('./routes/applications');
-app.use('/api/applications', applicationRoutes);
+const applicationRoutes = require("./routes/applications");
+app.use("/api/applications", applicationRoutes);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
